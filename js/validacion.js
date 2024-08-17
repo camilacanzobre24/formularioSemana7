74694c00cdc1c33255a4b70c06cada71483e6c0f
camilacanzobre24 <camilacanzobre@hotmail.com>
2024-08-17 11:27:43 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,17 @@
+document.getElementById('regBtn').addEventListener('click', function () {
+    let campos = document.getElementsByClassName("form-label");
+    let camposArreglo = Array.from(campos);
+    let hayCamposVacios = false;
+    camposArreglo.forEach(function(campo) {
+        if(campo.value.trim()=== ""){
+            hayCamposVacios = true;
+        }
+    });
+    if(hayCamposVacios){
+        showAlertError();
+    }
+});
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
